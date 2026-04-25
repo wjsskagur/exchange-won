6259e7b4a1911c6053338d178114230ade33278e
NAMHYEOK JEON <all_step@naver.com>
2026-04-25 13:07:46 +0900

    사용하지 않는 import 제거

--- src/main/java/com/switchwon/forex/domain/order/OrderRepository.java
+++ src/main/java/com/switchwon/forex/domain/order/OrderRepository.java
@@ -2,7 +2,6 @@ package com.switchwon.forex.domain.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
